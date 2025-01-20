6941b7298dad7f058d281ef42addea256022e623
coil398 <t.kawase398@gmail.com>
2025-01-21 02:23:17 +0900

    hello ruli

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Write};
+
+fn main() {
+    println!("Welcome to Rust Lisp, Ruli!");
+
+    loop {
+        print!("ruli> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim() == "exit" {
+            break;
+        }
+
+        println!("Input: {}", input.trim());
+    }
+}
